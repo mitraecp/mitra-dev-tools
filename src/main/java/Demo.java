cacc54944ba6db1d1f9cafc34f97affa2ab11df3
melqui <melquisedeque.silva@mitraecp.com>
2021-11-22 17:25:31 -0300

    mitra-dev-tools

---
+++ src/main/java/Demo.java
@@ -0,0 +1,12 @@
+import br.com.mitra.model.Cube;
+import br.com.mitra.model.Dimension;
+import br.com.mitra.model.Source;
+import br.com.mitra.service.*;
+import br.com.mitra.util.MitraDevTools;
+
+public class Demo implements MitraDevTools {
+
+    public void doExcute(Source source, Dimension dimension, Cube cube, MitraDevToolsCubeContentService cubeContentService, MitraDevToolsCubeService cubeService, MitraDevToolsDimensionContentService dimensionContentService, MitraDevToolsDimensionService dimensionService, MitraDevToolsSourceContentService sourceContentService) {
+
+    }
+}
